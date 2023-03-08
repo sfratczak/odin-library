83591c022529591c8533543479a51000a72c1f54
Szymon Fratczak <42326374+sfratczak@users.noreply.github.com>
2023-03-08 22:01:57 +0100

    Add html, css, js, readme files

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # odin-library
+
+This was made as an exercise in The Odin Project curriculum. A small Library app that stores books.
---
+++ javascript.js
@@ -0,0 +1,14 @@
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
+
+Book.prototype.info = function info() {
+  return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
+};
+
+const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, "not read yet");
+
+console.log(theHobbit.info());
