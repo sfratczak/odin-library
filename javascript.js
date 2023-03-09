924c586b3ea6eb7ed017d29bbdd62c9ae129eeee
Szymon Fratczak <42326374+sfratczak@users.noreply.github.com>
2023-03-09 08:22:55 +0100

    Add addBookToLibrary function

--- javascript.js
+++ javascript.js
@@ -1,8 +1,14 @@
+const myLibrary = [];
+
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
+}
+
+function addBookToLibrary(title, author, pages, read) {
+  myLibrary.push(new Book(title, author, pages, read));
 }
 
 Book.prototype.info = function info() {
